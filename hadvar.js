e62a028c25be15e8ea19fd8fb8671ea9c709a25f
Harrison Keeling <chasekeeling@me.com>
2019-04-23 21:07:43 -0700

    Add basic logger, boilerplate discd/tg singletons

---
+++ hadvar.js
@@ -0,0 +1,23 @@
+const { fork } = require('child_process');
+const fs = require('fs');
+const { logger } = require('./lib/utils.js');
+
+const TelegramBot = require('./lib/telegram.js');
+const DiscordBot = require('./lib/discord.js');
+
+// Read in the source configurations
+// TODO: Validation
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+const constructors = {
+	telegram: TelegramBot,
+	discord: DiscordBot
+};
+
+// Each connected source is a child process
+const sources = config.sources.map(source => instantiateSource(source));
+
+function instantiateSource(source) {
+	const child = new constructors[source.type](source);
+	return Object.assign(source, { instance: child });
+}
---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"PROJECT_DIR": __dirname,
+	"tokens": {
+		"telegram": "888636035:AAHUIqyAAJ0P749pzXCpt__oGbhxZw4iVDk",
+		"discord": "NTcwNDcyMDU2NTEzMzYzOTc4.XL_uyw.vbMbFqMB4MQ044vZBP38c9gQKRI"
+	}	
+};
